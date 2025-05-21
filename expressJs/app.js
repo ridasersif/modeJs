f877221909c7de277edb37425ae512b0f39e652d
ridasersif <ridasersif1@gmail.com>
2025-05-21 17:37:37 +0200

    comected to database

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3002
+
+/*D7b5I08t7eB16592*/
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3002;
 
 app.get('/', (req, res) => {
-  res.sendFile("./views/home.html",{root:__dirname})
-})
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose.connect('mongodb+srv://ridasersif1:D7b5I08t7eB16592@cluster0.potalny.mongodb.net/testDB?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('Connected to MongoDB Atlas!');
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Connection error:', err);
+  });
